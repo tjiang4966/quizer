d3374f5c6802e520a7247ef7094e4114beb479cc
Jeremy Jiang <tjiang4966@gmail.com>
2021-05-29 01:47:11 -0400

    created a demo quiz page

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
 </script>
 <style lang="scss">
 #app {
-  background-color: $app-white;
+  background-color: $app-maize;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 <div class="homeview">
   <img alt="Vue Logo" src="../assets/logo.png"/>
   <h1>开动脑筋吧</h1>
-  <button class="app-btn success"><router-link to="">点我开始</router-link></button>
+  <button class="app-btn primary"><router-link to="/quiz">点我开始</router-link></button>
 </div>
 </template>
 <script>
