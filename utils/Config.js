bd44d1fc7d5a80086ca74d7df740fc439df77170
Jeremy Jiang <tjiang4966@gmail.com>
2021-05-31 23:20:45 -0400

    use env config

--- utils/Config.js
+++ utils/Config.js
@@ -9,12 +9,16 @@ const config_path = path.join(__dirname, '../config/config.yaml')
 const Config = {
   get: (key) => {
     // Get document, or throw exception on error
-    try {
-      const doc = yaml.load(fs.readFileSync(config_path))
-      return doc[key]
-    } catch (e) {
-      console.log(e)
-      return null
+    if (process.env.ENVIRONMENT == 'production' || process.env.ENVIRONMENT == 'staging') {
+      return process.env[key]
+    } else {
+      try {
+        const doc = yaml.load(fs.readFileSync(config_path))
+        return doc[key]
+      } catch (e) {
+        console.log(e)
+        return null
+      }
     }
   }
 }
